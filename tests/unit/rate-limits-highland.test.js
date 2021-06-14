092d2861a5b4678b1ed2655a7d691dbc606e6054
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2021-06-14 18:06:59 +0200

    ok, nailed it

--- tests/unit/rate-limits-highland.test.js
+++ tests/unit/rate-limits-highland.test.js
@@ -2,8 +2,10 @@ const __ = require('highland')
 
 const asyncFn = jest.fn().mockImplementation(async input => ({time: Date.now()}))
 
-test('async basic', done => {
-  const step = 100
+const step = 100
+const tolerance = 10
+
+test('ratelimit async sequence', done => {
   __([1, 2])
     .ratelimit(1, step)
     .map(asyncFn)
@@ -11,9 +13,23 @@ test('async basic', done => {
     .pluck('time')
     .toArray(timestamps => {
       const delay = timestamps[1] - timestamps[0]
+      expect(delay >= step).toBe(true)
+      expect(delay < step + tolerance).toBe(true)
+      done()
+    })
+})
+
+test('ratelimit async parallel - same', done => {
+  __([1, 2])
+    .ratelimit(1, step)
+    .map(asyncFn)
+    .map(__).parallel(2)
+    .pluck('time')
+    .toArray(timestamps => {
+      const delay = timestamps[1] - timestamps[0]
       console.log(delay)
       expect(delay >= step).toBe(true)
-      expect(delay < step + 10).toBe(true)
+      expect(delay < step + tolerance).toBe(true)
       done()
     })
 })
