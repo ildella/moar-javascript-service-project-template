b659233a6d26620e45d5dc003d895ff8073f4037
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2022-04-21 14:46:37 +0200

    the item schema for mongo

--- tests/mongodb/global-setup.js
+++ tests/mongodb/global-setup.js
@@ -1,21 +1,21 @@
 const mongoTestBase = require('./mongo-test-base')
-// const {tenantDatabase} = require('../../src/mongodb/mongodb')
-// const schema = require('../../src/mongodb/items-schema')
+const {createCollection, tenantDatabase} = require('../../src/mongodb/mongodb')
+const schema = require('../../src/mongodb/item-schema')
 
 const {
   start,
   stop,
   serverUri,
-  // connection,
+  connection,
 } = mongoTestBase()
 
 module.exports = async () => {
   await start()
 
-  // const activeConnection = await connection()
-  // const database = tenantDatabase(activeConnection)
+  const activeConnection = await connection()
+  const database = tenantDatabase(activeConnection)
   // console.log(await database.stats())
-  // await createCollection(database, 'items', schema)
+  await createCollection(database, 'items', schema)
 
   process.env.mongoUri = serverUri()
   global.stop = stop
