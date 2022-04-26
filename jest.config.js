d998dcfe2416caf9d31604aeb38ac0b37172ee93
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2022-04-26 17:09:35 +0200

    using jest preset and eslint config from test-commons now

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,11 @@
     https://jestjs.io/docs/configuration#projects-arraystring--projectconfig
 */
 
+const {jestCommons} = require('ildella-test-commons')
+const {jestPreset} = jestCommons
+
 module.exports = {
+  ...jestPreset,
   projects: [
     '<rootDir>/tests/unit',
     '<rootDir>/tests/integration',
