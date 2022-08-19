8995797bb36d9f8ed4a831a7a90c5038e776c821
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2022-08-19 10:20:34 +0200

    npm publish must be here

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,6 @@ echo releasing "v$version"
 
 git tag "v$version"
 git push origin "v$version"
-# npm publish
 ## https://cli.github.com/manual/gh_release_create
 gh release create "v$version"
+npm publish
