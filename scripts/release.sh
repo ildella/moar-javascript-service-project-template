166ee9dd6b7c8499a80e413e180af7422bcb948f
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2022-12-09 19:52:33 +0100

    proper release script finally migrated here as well..

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-set -euxo
+set -euo pipefail
 
-version=$(cat "package.json" |jq -r .version)
-echo releasing "v$version"
+version=$(< "package.json" jq -r .version)
+tag=v$version
 
-git tag "v$version"
-git push origin "v$version"
-## https://cli.github.com/manual/gh_release_create
-gh release create "v$version"
-npm publish
+echo releasing "$tag"
+
+# https://cli.github.com/manual/gh_release_create
+gh release create "$tag"
